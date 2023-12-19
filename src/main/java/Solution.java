ba4b72f1531183a37c033c4bb4225c565c52156b
alek <wait82985@gmail.com>
2023-12-19 22:47:57 +0600

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,5 +1,61 @@
+import java.util.*;
+
 public class Solution {
     public int networkBecomesIdle(int[][] edges, int[] patience) {
-        return 0;
+        int n = patience.length;
+        List<List<Integer>> adjList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adjList.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < edges.length; i++) {
+            adjList.get(edges[i][0]).add(edges[i][1]);
+            adjList.get(edges[i][1]).add(edges[i][0]);
+        }
+        int[] distFromStart = bfs(adjList, n);
+
+        int max = -1;
+        for (int i = 1; i < n; i++) {
+            int currentServerRequestTime = distFromStart[i];
+            if(currentServerRequestTime * 2 < patience[i]){
+                max = Math.max(max, currentServerRequestTime * 2 + 1);
+            }else {
+                int requestResponseTime = currentServerRequestTime * 2;
+                    int startingTimeOfLastRetry = requestResponseTime % patience[i] == 0
+                            ? requestResponseTime - patience[i]
+                            : requestResponseTime - requestResponseTime % patience[i];
+                max = Math.max(max, requestResponseTime + startingTimeOfLastRetry + 1);
+                }
+
+
+        }
+
+        return max;
+    }
+    private int[] bfs(List<List<Integer>> adjList, int n){
+        int[] resultList = new int[n];
+        Arrays.fill(resultList, -1);
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(0);
+        resultList[0] = 0;
+        int level = 0;
+
+
+        while (!queue.isEmpty()){
+            level++;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int cur = queue.poll();
+                for (int j = 0; j < adjList.get(cur).size(); j++) {
+                    if( resultList[adjList.get(cur).get(j)] == -1){
+                        resultList[adjList.get(cur).get(j)] = level;
+                        queue.offer(adjList.get(cur).get(j));
+                    }
+                }
+            }
+        }
+        return resultList;
     }
 }
+
+
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -28,4 +28,60 @@ public class SolutionTest {
 
         Assert.assertEquals(expected, actual);
     }
+    @Test
+    public void test3() {
+        int[][] edges = {
+                {3, 8},
+                {4, 13},
+                {0, 7},
+                {0, 4},
+                {1, 8},
+                {14, 1},
+                {7, 2},
+                {13, 10},
+                {9, 11},
+                {12, 14},
+                {0, 6},
+                {2, 12},
+                {11, 5},
+                {6, 9},
+                {10, 3}
+        };
+        int[] patience = {0,3,2,1,5,1,5,5,3,1,2,2,2,2,1};
+        int expected = 20;
+        int actual = new Solution().networkBecomesIdle(edges, patience);
+
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void test4() {
+        int[][] edges = {
+                {5, 7},
+                {15, 18},
+                {12, 6},
+                {5, 1},
+                {11, 17},
+                {3, 9},
+                {6, 11},
+                {14, 7},
+                {19, 13},
+                {13, 3},
+                {4, 12},
+                {9, 15},
+                {2, 10},
+                {18, 4},
+                {5, 14},
+                {17, 5},
+                {16, 2},
+                {7, 1},
+                {0, 16},
+                {10, 19},
+                {1, 8}
+        };
+        int[] patience = {0,2,1,1,1,2,2,2,2,1,1,1,2,1,1,1,1,2,1,1};
+        int expected = 67;
+        int actual = new Solution().networkBecomesIdle(edges, patience);
+
+        Assert.assertEquals(expected, actual);
+    }
 }
